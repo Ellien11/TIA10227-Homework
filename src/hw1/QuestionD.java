ba850925e0bf402e673fda10e08ee7c3bcb91f25
Ellie <vous32bien1@gmail.com>
2024-04-23 21:26:24 +0800

    modified hw2

--- src/hw1/QuestionD.java
+++ src/hw1/QuestionD.java
@@ -2,11 +2,12 @@ package hw1;
 
 public class QuestionD {
 	public static void main(String[] args) {
-		double pi = 3.1415;
+		final double PI = 3.1415;
 		int i = 5;
 		
-		System.out.println("圓面積" + ":" + i * i * pi);
-		System.out.println("圓周長" + ":" + 2 * i * pi);
+		System.out.println("圓面積=" + i * i * PI);
+		System.out.println("圓周長=" + 2 * i * PI);
+	
 		
 	}
 
