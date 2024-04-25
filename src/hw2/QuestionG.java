63328e7c1491894ffc03661c26f1ee108b7be50c
Ellie <vous32bien1@gmail.com>
2024-04-25 21:50:42 +0800

    add questiong

--- src/hw2/QuestionG.java
+++ src/hw2/QuestionG.java
@@ -3,9 +3,15 @@ package hw2;
 public class QuestionG {
 	public static void main(String[] args) {
 		// 請設計一隻Java程式，輸出結果為以下A-FFFFFFF升冪
-		
-		
+		int c, d;
+		for (c = 1; c <= 6; c++) {
+			for (d = 1; d <= c; d++) {
+				System.out.print((char) (64 + c));
+
+			}
+			System.out.println();
 		}
 
 	}
 
+}
