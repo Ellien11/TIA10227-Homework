0725f89fd2e31366798eb03df3ac0acc613fdae4
Ellie <vous32bien1@gmail.com>
2024-05-02 13:18:23 +0800

    modify hw3

--- src/hw3/QuestionC.java
+++ src/hw3/QuestionC.java
@@ -3,6 +3,10 @@ package hw3;
 import java.util.Scanner;
 
 public class QuestionC {
+	/*
+	 * 阿文很喜歡簽大樂透(1～49)，但他是個善變的人，上次討厭數字是4，但這次他想要依心情決定討厭哪個數字，
+	 * 請您設計一隻程式，讓阿文可以輸入他不想要的數字(1～9)，畫面會顯示他可以選擇的號碼與總數，如圖：
+	 */
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
